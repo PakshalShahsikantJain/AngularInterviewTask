aa2b08e9d26450d87f223065585179c319c8b361
Pakshal Shashikant Jain <80408249+PakshalShahsikantJain@users.noreply.github.com>
2024-07-17 12:06:24 +0530

    commit

--- FrontEnd/src/app/loading/loading.component.ts
+++ FrontEnd/src/app/loading/loading.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="loading-overlay">
       <mat-spinner class="spinner"></mat-spinner>
-      <h1 class="text">Ayurvedic Products Store Loading Soon.<br> Sorry To Keep You Waiting..!!</h1>
+      <h1 class="text">Web Store Loading Soon.<br> Sorry To Keep You Waiting..!!</h1>
     </div>
   `,
   styleUrls: ['./loading.component.css']
